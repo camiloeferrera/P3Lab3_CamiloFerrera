178ed4f8b90548ab1d19ceb5340ec8df821c2909
camiloeferrera <camiloferrera@hotmail.com>
2020-05-15 17:31:57 -0600

    Ya casi

--- Lab3_CamiloFerrera.cpp
+++ Lab3_CamiloFerrera.cpp
@@ -8,7 +8,13 @@ void conway (int**&,int,int,int);
 void ImprimirMatriz (int** matriz, int filas, int columnas){
 	for (int i = 0; i < filas; i++){
 		for (int j = 0; j < columnas;j++){
-			cout << "[" << matriz[i][j] << "] ";
+			if (matriz[i][j] == 3){
+				cout << "[#] ";
+			} else if (matriz[i][j] == 1){
+				cout << "[*] ";
+			} else {
+				cout << "[ ] ";
+			}
 		}
 		cout << endl;
 	}
@@ -76,60 +82,67 @@ int main (){
 
 void conway (int**& matriz,int filas, int columnas, int turnos){
 	int cont = 0;
-			
+	char avance;		
 	
-	int **matrizflag = matriz;
+	int matrizflag [filas][columnas];
+	for (int i = 0; i < filas ; i++){
+		for (int j = 0; j < columnas ; j++){
+			matrizflag[i][j] = matriz[i][j];
+		}
+	}
 	int contvivas = 0;
 	while (cont < turnos){
 		
-		for (int i = 1; i < filas-1; i++){
-			for (int j = 1; j < columnas-1; j++){
+		for (int i = 0; i < filas; i++){
+			for (int j = 0; j < columnas; j++){
 				contvivas = 0;
-				
-				if (matriz[i][j+1] == 1){
+				if (matriz[i][j] == 1 || matriz[i][j] == 0){
+					if (matriz[i][j+1] == 1){
 					contvivas++;
-				}
-				if (matriz[i][j-1] == 1){
-					contvivas++;
-				}
-				if (matriz[i-1][j] == 1){
-					contvivas++;
-				}
-				if (matriz[i-1][j+1] == 1){
-					contvivas++;
-				}
-				if (matriz[i-1][j-1] == 1){
-					contvivas++;
-				}
-				if (matriz[i+1][j] == 1){
-					contvivas++;
-				}
-				if (matriz[i+1][j+1] == 1){
-					contvivas++;
-				}
-				if (matriz[i+1][j-1] == 1){
-					contvivas++;
-				}
-				
-				
-				
-				
-				if (matriz[i][j] == 1){
-					if (contvivas == 2 || contvivas == 3){
-						matrizflag[i][j] = 1;
-					} else {
-						matrizflag[i][j] = 0;
-					}					
-				} else if (matriz[i][j] == 0) {
-					if (contvivas == 3){
-						matrizflag[i][j] = 1;
+					}
+					if (matriz[i][j-1] == 1){
+						contvivas++;
+					}
+					if (matriz[i-1][j] == 1){
+						contvivas++;
+					}
+					if (matriz[i-1][j+1] == 1){
+						contvivas++;
+					}
+					if (matriz[i-1][j-1] == 1){
+						contvivas++;
+					}
+					if (matriz[i+1][j] == 1){
+						contvivas++;
+					}
+					if (matriz[i+1][j+1] == 1){
+						contvivas++;
+					}
+					if (matriz[i+1][j-1] == 1){
+						contvivas++;
+					}
+					
+					if (matriz[i][j] == 1){
+						if (contvivas == 2 || contvivas == 3){
+							matrizflag[i][j] = 1;
+						} else {
+							matrizflag[i][j] = 0;
+						}					
+					} else if (matriz[i][j] == 0) {
+						if (contvivas == 3){
+							matrizflag[i][j] = 1;
+						}
 					}
 				}
-				cout << contvivas << endl;
+									
 			}
 		}		
 		
-		matriz = matrizflag;
+		for (int i = 0; i < filas ; i++){
+			for (int j = 0; j < columnas ; j++){
+				matriz[i][j] = matrizflag[i][j];
+			}
+		}
 		ImprimirMatriz(matriz,filas,columnas);
 		cout << endl;
 		
